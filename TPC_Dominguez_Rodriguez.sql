e575f566f75a8cfdfe046cabb3b0b5447114fab9
rodriguezleandro98 <leandrohodriguez98@gmail.com>
2020-11-10 20:31:16 -0300

    modificacion bbdd

--- TPC_Dominguez_Rodriguez.sql
+++ TPC_Dominguez_Rodriguez.sql
@@ -1,3 +1,6 @@
+
+--IMPORTANTE, SE CAMBIO LAS TABLAS A PLURAL, SE AGREGA FECHA A COMENTARIO, SE CREO TABLA PRIORIDADES.
+
 use master
 go
 drop database TPC_Dominguez_Rodriguez
@@ -16,12 +19,12 @@ CREATE TABLE Usuarios(
 	Estado BIT NOT NULL,
 )
 GO
-CREATE TABLE TipoUsuario(
+CREATE TABLE TipoUsuarios(
 	ID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
 	Nombre VARCHAR(50) NOT NULL
 )
 GO
-CREATE TABLE Cliente(
+CREATE TABLE Clientes(
 	ID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
 	Nombres VARCHAR(50) NOT NULL,
 	Apellidos VARCHAR(50) NOT NULL,
@@ -31,11 +34,12 @@ CREATE TABLE Cliente(
 	FechaReg date NOT NULL
 )
 GO
-CREATE TABLE Solicitud(
+CREATE TABLE Solicitudes(
 	ID BIGINT NOT NULL PRIMARY KEY IDENTITY(1,1),
 	IDCliente INT NOT NULL,
 	IDUsuario BIGINT NOT NULL,
 	IDSintoma INT NOT NULL,
+	IDPrioridad INT NOT NULL,
 	Titulo VARCHAR(100),
 	Descripcion VARCHAR(500),
 	IDEstado INT NOT NULL,
@@ -47,10 +51,11 @@ CREATE TABLE Comentarios(
 	ID BIGINT NOT NULL PRIMARY KEY IDENTITY(1,1),
 	IDSolicitud BIGINT NOT NULL,
 	IDUsuario BIGINT NOT NULL,
+	FechaComent date not null,
 	Comentario VARCHAR(500) NOT NULL
 )
 GO
-CREATE TABLE EstadosdeS(
+CREATE TABLE Estado_de_Solicitud(
 	ID INT NOT NULL PRIMARY KEY IDENTITY (1,1),
 	Nombre VARCHAR(50) NOT NULL
 )
@@ -61,26 +66,33 @@ CREATE TABLE Sintomas(
 	Estado bit not null
 )
 GO
+CREATE TABLE Prioridades(
+	ID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+	Nombre VARCHAR(10) NOT NULL,
+)
 
 /* Restricciones */
 -- Foreign Key
 Alter Table Usuarios
-Add constraint FK_TipoUsuario Foreign Key(IDTipodeusuario) references TipoUsuario(ID)
+Add constraint FK_TipoUsuarios Foreign Key(IDTipodeusuario) references TipoUsuarios(ID)
 go
-Alter Table Solicitud
-Add constraint FK_Cliente Foreign Key(IDCliente) references Cliente(ID)
+Alter Table Solicitudes
+Add constraint FK_Clientes Foreign Key(IDCliente) references Clientes(ID)
 go
-Alter Table Solicitud
-Add constraint FK_Usuario Foreign Key(IDUsuario) references Usuarios(ID)
+Alter Table Solicitudes
+Add constraint FK_Usuarios Foreign Key(IDUsuario) references Usuarios(ID)
 go
-Alter Table Solicitud
-Add constraint FK_Sintoma Foreign Key(IDSintoma) references Sintomas(ID)
+Alter Table Solicitudes
+Add constraint FK_Sintomas Foreign Key(IDSintoma) references Sintomas(ID)
 go
-Alter Table Solicitud
-Add constraint FK_Estado Foreign Key(IDEstado) references EstadosdeS(ID)
+Alter Table Solicitudes
+Add constraint FK_Estado Foreign Key(IDEstado) references Estado_de_Solicitud(ID)
+go
+Alter Table Solicitudes
+Add constraint FK_Prioridades Foreign Key(IDPrioridad) references Prioridades(ID)
 go
 Alter Table Comentarios
-Add constraint FK_IDSolicitud Foreign Key(IDSolicitud) references Solicitud(ID)
+Add constraint FK_IDSolicitud Foreign Key(IDSolicitud) references Solicitudes(ID)
 go
 Alter Table Comentarios
 Add constraint FK_IDUsuario Foreign Key(IDUsuario) references Usuarios(ID)
